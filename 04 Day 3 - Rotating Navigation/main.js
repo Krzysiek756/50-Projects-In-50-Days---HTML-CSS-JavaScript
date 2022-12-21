f5a61891999bf8008c556b8a4fbc648abf3b7883
Krzysiek756 <114465842+Krzysiek756@users.noreply.github.com>
2022-12-21 14:06:36 +0100

    update

--- 04 Day 3 - Rotating Navigation/main.js
+++ 04 Day 3 - Rotating Navigation/main.js
@@ -1,10 +1,7 @@
 const open = document.getElementById('open');
 const close = document.getElementById('close');
-const container = document.querySelector('.container'); // pobieranie elementów
+const container = document.querySelector('.container'); 
 
 
-open.addEventListener('click', () => container.classList.add('show-nav')); // funkcja strzałkowa ustawiona na nasłuchiwanie na button
-// dodajaca klase show-nav// 
-
-close.addEventListener('click', () => container.classList.remove('show-nav')); // funkcja strzałkowa ustawiona na nasłuchiwanie na button
-// usuwajaca klase show-nav
+open.addEventListener('click', () => container.classList.add('show-nav')); 
+close.addEventListener('click', () => container.classList.remove('show-nav')); 
