2be52cc97572a1a3a8bf03e8f81bf3b4806386df
Krzysztof Siek <Krzysztof.Siek93@gmail.com>
2022-12-21 14:04:50 +0100

    Hidden search widget

--- 04 Day 3 - Rotating Navigation/main.js
+++ 04 Day 3 - Rotating Navigation/main.js
@@ -1,9 +1,10 @@
 const open = document.getElementById('open');
 const close = document.getElementById('close');
-const container = document.querySelector('.container');
+const container = document.querySelector('.container'); // pobieranie elementów
 
 
-open.addEventListener('click', () => container.classList.add('show-nav'));
+open.addEventListener('click', () => container.classList.add('show-nav')); // funkcja strzałkowa ustawiona na nasłuchiwanie na button
+// dodajaca klase show-nav// 
 
-
-close.addEventListener('click', () => container.classList.remove('show-nav'));
+close.addEventListener('click', () => container.classList.remove('show-nav')); // funkcja strzałkowa ustawiona na nasłuchiwanie na button
+// usuwajaca klase show-nav
