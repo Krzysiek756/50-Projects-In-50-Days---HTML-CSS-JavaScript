57459fb1b118792c0c437382628cb367f8e37777
Krzysztof Siek <Krzysztof.Siek93@gmail.com>
2022-12-22 12:22:18 +0100

    start project

--- 06 Day 5 - Blurry Loading/main.js
+++ 06 Day 5 - Blurry Loading/main.js
@@ -1,36 +1,37 @@
 const loadText = document.querySelector('.loading-text');
-const bG = document.querySelector('.bg');
+const bG = document.querySelector('.bg'); // pobieranie przyciskow 
 
-let load = 0;
+let load = 0; // zmienna licznik load ustaiwiona od zera 
 
-let int = setInterval(blurring, 30);
+let int = setInterval(blurring, 30); // na zmienna int jest przypisany interval funkcji , druga wlasciwosc to ms
 
-function blurring() {
-    load++;
 
-    if (load > 99) {
-        clearInterval(int);
+//Gdy odpalimy nasz skrypt, wykonuje się on natychmiast - linia po linii.
+//W JavaScript istnieją też asynchroniczne funkcje, które pozwalają odpalić nasz kod z opóźnieniem czasowym, lub pozwalają odpalać taki kod //cyklicznie co jakiś czas.
+
+function blurring() { // funckja ładująca blur 
+    load++; // licznik ++
+
+    if (load > 99) { // jeżeli load dojdzie do 100 
+        clearInterval(int); // wyczysci interwal int
 
     }
 
+    loadText.innerText = `${load}%`; // wczytuje text w diva
+    loadText.style.opacity = scale(load, 0, 100, 1, 0); // zmienna stylów diva
+    bG.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`; // zmienna stylów diva
 
 
-    loadText.innerText = `${load}%`;
-    loadText.style.opacity = scale(load, 0, 100, 1, 0);
-    bG.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
-
-
-
-
-    console.log(load)
-    console.log(loadText.style.opacity)
-    console.log(bG.style.filter)
 }
 
 
+
+//JavaScript / jQuery - mapuj zakres liczb na inny zakres liczb
 const scale = (number, inMin, inMax, outMin, outMax) => {
     return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
+
+
 
 
 
