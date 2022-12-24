5aed27ba4f76a3fad422bd5d95b6baff20e0c936
Krzysztof Siek <Krzysztof.Siek93@gmail.com>
2022-12-24 11:29:35 +0100

    project finish

---
+++ 09 Day 8 - Form Wave Animation/dist/main.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var labels = document.querySelectorAll('.form-control label');
+labels.forEach(function (label) {
+  label.innerHTML = label.innerText.split('').map(function (letter, idx) {
+    return "<span style=\"transition-delay:".concat(idx * 50, "ms\">").concat(letter, "</span>");
+  }).join('');
+});
