f56f4e9b2c5c27efbe88f0af47e7fdc2db2ad47d
Krzysztof Siek <Krzysztof.Siek93@gmail.com>
2022-12-30 11:01:19 +0100

    finish

---
+++ 13 Day 12 - FAQ Collapse/main.js
@@ -0,0 +1,8 @@
+const toggles = document.querySelectorAll('.faq__toggle');
+
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active');
+    })
+})
