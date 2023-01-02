d216056db16ffc1a7ed5ccb3e8a0a482d2b46cac
Krzysztof Siek <Krzysztof.Siek93@gmail.com>
2023-01-02 21:22:07 +0100

    commit

---
+++ 14 Day 13 - Random Choice Picker/main.js
@@ -0,0 +1,25 @@
+const tagsEl = document.getElementById('tags');
+const taxtarea = document.getElementById('textarea');
+
+
+taxtarea.focus()
+
+
+
+taxtarea.addEventListener('keyup', (e) => {
+    createTags(e.target.value)
+})
+
+function createTags(input) {
+    const tags = input.split(',').filter(tag => tag.trim() !==
+        '').map(tag => tag.trim())
+
+    tagsEl.innerHTML = '';
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span');
+        tagEl.classList.add('tag');
+        tagEl.innerText = tag;
+        tagsEl.appendChild(tagEl)
+    });
+}
