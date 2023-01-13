8ca26777989335665443c34d8455eb85a408a416
Krzysztof Siek <Krzysztof.Siek93@gmail.com>
2023-01-13 12:19:41 +0100

    finish clock

--- 20 Day 19 - Theme Clock/main.js
+++ 20 Day 19 - Theme Clock/main.js
@@ -2,11 +2,11 @@ const hourEl = document.querySelector('.hour');
 const minuteEl = document.querySelector('.minute');
 const secondEl = document.querySelector('.second');
 const timeEl = document.querySelector('.time');
-const dateEl = document.querySelector('.data');
+const dateEl = document.querySelector('.date');
 const toggle = document.querySelector('.toggle');
 
 
-const days = ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota', 'Niedziela'];
+const days = ['Niedziela', 'Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota'];
 
 const months = ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Pazdziernik', 'Listopad', 'Grudzien'];
 
@@ -34,6 +34,7 @@ function setTime() {
     const day = time.getDay();
     const date = time.getDate();
     const hours = time.getHours();
+    const year = time.getFullYear();
     const hoursForClock = hours % 12;
     const minutes = time.getMinutes();
     const seconds = time.getSeconds();
@@ -49,19 +50,25 @@ function setTime() {
 
     timeEl.innerHTML = `${hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
 
-    dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`;
+    dateEl.innerHTML = `${days[day]}, <div class="circle">${date}</div>, ${months[month]}, ${year}`;
+
+    // console.log(days[day], months[month], {
+    //     date
+    // });
 
 
 
 }
 
-setTime()
 
 
 
 function scale(number, inMin, inMax, outMin, outMax) {
     return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
+
+
+setTime()
 
 
 setInterval(setTime, 1000);
